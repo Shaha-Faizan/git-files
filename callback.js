044c9f04196a97e53e934aa176e9d788e88d861c
shaha-Faizan <faizanshaha21@gmail.com>
2024-09-19 15:51:22 +0530

    delete

--- callback.js
+++
@@ -1,8 +0,0 @@
-function attacheventlistner() {
-    let count = 0;
-    document.getElementById("click").addEventListener("click", function xyz () {
-        console.log("button clicked", ++count)
-    })
-}
-
-attacheventlistner()
